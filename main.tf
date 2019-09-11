059f4f762161e56b025d20a713119549906fc0cd
csa3 <csa3@localhost>
2019-09-11 11:44:25 +0100

    changed SSH source address allowed

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_security_group" "aws-security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["52.51.152.178/32"]
   }
 
   # HTTP access from the VPC
